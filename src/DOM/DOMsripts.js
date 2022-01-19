2ab7bdd907f2919202a63d5aa00ce9fcb81fa0cf
Dvonne Williams <dvonnewilliams1@gmail.com>
2022-01-18 18:33:20 -0700

    added image nav bar and made it clickable to each image

--- src/DOM/DOMsripts.js
+++ src/DOM/DOMsripts.js
@@ -1,4 +1,3 @@
-import selfie from './images/me.jpeg'
 import us from './images/dweebandme.jpeg'
 import dog from './images/sasha.jpeg'
 import teton1 from './images/teton.jpeg'
@@ -26,36 +25,69 @@ class DOMcontroller {
 
     createImageSlider(){
         const imageSlider = document.querySelector('.image-slider')
-        const pictures = [selfie, us, dog, teton1, teton2, skiing]
+        const imageNavBar = document.querySelector('.image-nav-bar')
+        const pictures = [us, dog, teton1, teton2, skiing]
         const next = document.querySelector('.next')
         const prev = document.querySelector('.prev')
 
         pictures.forEach((picture, i) =>{
             const myImage = new Image()
+            const navDot = document.createElement('button')
+
             myImage.src = picture
+            myImage.dataset.index = i
+
             if (i == 0){
                 myImage.className = `image main`
             }
             else{
                 myImage.className = `image`
             }
-            myImage.dataset.index = i
             this.images.push(myImage)
             imageSlider.appendChild(myImage)
+
+            navDot.dataset.index = i
+
+            if (i == 0){
+                navDot.className = 'image-nav-dot active'
+            }
+            else{
+                navDot.className = 'image-nav-dot'
+            }
+
+            imageNavBar.appendChild(navDot)
+
+            navDot.addEventListener('click', ()=>{
+                this.changeImage(i)
+            })
+        
         })
 
         next.addEventListener('click', ()=>{
             const index = document.querySelector('.main').dataset.index
-            this.changeImage('next', index)
+            this.cycleImage('next', index)
         })
 
         prev.addEventListener('click', ()=>{
             const index = document.querySelector('.main').dataset.index
-            this.changeImage('prev', index)
+            this.cycleImage('prev', index)
         }) 
     }
 
-    changeImage(direction, index){
+    changeImage(index){
+        const currentIndex = document.querySelector('.active').dataset.index
+        const navDots = document.querySelectorAll('.image-nav-dot')
+
+        this.images[currentIndex].classList.remove('main')
+        this.images[index].classList.add('main')
+
+        navDots[currentIndex].classList.remove('active')
+        navDots[index].classList.add('active')
+    }
+
+    cycleImage(direction, index){
+
+        const navDots = document.querySelectorAll('.image-nav-dot')
         
         if (direction == 'next'){
             index++
@@ -65,16 +97,17 @@ class DOMcontroller {
         }
         else {
             if (index == 0){
-                index = this.images.length-1
+                index = this.images.length
             }
             index--
         }
 
         for (let i=0; i < this.images.length; i++){
             this.images[i].classList.remove('main')
+            navDots[i].classList.remove('active')
         }   
         this.images[index].classList.add('main')
-        console.log(this.images[index])
+        navDots[index].classList.add('active')
     }
 
 }
